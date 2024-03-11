a58031bc5cbe6e2e0960c6a48b349a9d19e397a8
ShivarajHanchinal <01fe22bca049@kletech.ac.in>
2024-03-11 16:11:11 +0530

    code related to employee_details

---
+++ employee_details.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class employee_details extends p {
+    void displaydetails(){
+    System.out.println("This is sub branch");
+    System.out.println("Employee details:");
+    System.out.println("Employee name:"+Person.emp_name);
+    System.out.println("Employee dob:"+Person.emp_dob);
+    System.out.println("Employee id:"+Person.emp_id);
+    System.out.println("Employee salary:"+Person.emp_salary);
+}
+}
